d0d78702a2fe8ca8372e7cf9fd346796c3e9531d
shantanupanda <shantanu.panda@talentica.com>
2016-02-11 13:09:50 +0530

    Initial

---
+++ recipes/machine_cluster.rb
@@ -0,0 +1,32 @@
+# Cookbook Name:: tal_ec2
+# Recipe:: machine_cluster
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+require 'chef/provisioning/aws_driver'
+
+aws = JSON.parse(File.read("/home/anshulp/hosted_chef/cookbooks/aws_machine/docs/mach_cluster.json"))
+
+with_driver "aws::#{aws['region']}"do
+
+
+with_machine_options :ssh_username => "#{aws['ssh_username']}",
+	:bootstrap_options => {
+		:key_name => "#{aws['key_name']}",
+		:instance_type => "#{aws['instance_type']}",
+		:associate_public_ip_address => "#{aws['public_ip']}",
+		:image_id => "#{aws['image_id']}",
+		:security_group_ids => "#{aws['security_group_ids']}",
+		:subnet_id => "#{aws['subnet_id']}"
+	}
+
+
+#Cluster Creation
+machine_batch  do
+	1.upto(aws['count']) do |count|
+		machine "#{aws['machine']['name']}_#{count}"do
+			recipe "#{aws['machine']['recipe']}"
+		end
+	end
+end
+end
